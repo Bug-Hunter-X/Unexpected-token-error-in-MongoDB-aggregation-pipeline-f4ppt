18951ae21cecec9e5847720efd4a8cc77538d16d
BugHunter-X <askzenai@gmail.com>
2025-02-02 18:24:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+```javascript
+// Assuming you have a MongoDB collection named 'users'
+
+db.users.aggregate([
+  {
+    $match: { age: { $gt: 25 } }
+  },
+  {
+    $group: {
+      _id: "$city",
+      averageAge: { $avg: "$age" },
+      totalCount: { $sum: 1 }
+    }
+  }
+])
+```
